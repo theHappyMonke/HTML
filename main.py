d952fedc5aadecebdaad5f45bb1fe2366025219f
theHappyMonke <23DelmarAS271@collyers.ac.uk>
2024-01-04 15:16:19 +0000

    pages!!!

--- main.py
+++ main.py
@@ -9,3 +9,22 @@ def index():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/page1')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
