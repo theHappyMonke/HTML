9572d7c6698d47e458aec6196ff9f21a397fee2b
theHappyMonke <23DelmarAS271@collyers.ac.uk>
2024-01-29 10:11:25 +0000

    made more javascript program

--- static/javascript/default_functions.js
+++ static/javascript/default_functions.js
@@ -3,3 +3,13 @@ const num1 = 5;
 const num2 = 3;
 const sum = num1 + num2;
 console.log(sum);
+
+count = 0;
+
+while (true) {
+    console.log("Hello, world!");
+    count = count + 1;
+    if (count === 5) {
+        break;
+    }
+}
