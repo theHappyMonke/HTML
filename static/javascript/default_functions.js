5c1abe8766f4ad41062fcb8f2c88904a931f89e3
theHappyMonke <23DelmarAS271@collyers.ac.uk>
2024-02-05 11:05:23 +0000

    changed javascript

--- static/javascript/default_functions.js
+++ static/javascript/default_functions.js
@@ -1,4 +1,5 @@
 // I do not know how to use javascript.
+// I am just trying to learn it.
 const num1 = 5;
 const num2 = 3;
 const sum = num1 + num2;
@@ -6,10 +7,24 @@ console.log(sum);
 
 count = 0;
 
+// while loop
 while (true) {
     console.log("Hello, world!");
-    count = count + 1;
+    count++;
     if (count === 5) {
         break;
     }
+}
+
+// for loop
+for (let i = 0; i < 5; i++) {
+    console.log("Hello, world!");
+}
+
+// if-else statement
+let age = input("Enter your age: ");
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a child.");
 }
