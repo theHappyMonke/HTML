22ac880139dda345993186f5102396e21600f4aa
theHappyMonke <23DelmarAS271@collyers.ac.uk>
2024-02-26 13:29:14 +0000

    added a button that will eventually show and remove the navigation bar.

--- static/javascript/default_functions.js
+++ static/javascript/default_functions.js
@@ -1,30 +1,8 @@
-// I do not know how to use javascript.
-// I am just trying to learn it.
-const num1 = 5;
-const num2 = 3;
-const sum = num1 + num2;
-console.log(sum);
-
-// while loop
-whileLoop = () => {
-    let i = 0;
-    while (i <= 5) {
-        console.log(i);
-        i++;
+function navToggle() {
+    const x = document.getElementById("navbar");
+    if (x.innerHTML === "Closed") {
+        x.innerHTML = "Open";
+    } else {
+        x.innerHTML = "Closed";
     }
 }
-
-// for loop
-forLoop = () => {
-    for (let i = 5; i >= 0; i--) {
-        console.log(i);
-    }
-}
-
-// if-else statement - not working
-//const age = document.getElementById("age").value;
-//if (age >= 18) {
-//    console.log("You are an adult.");
-//} else {
-//    console.log("You are a child.");
-//}
---
+++ static/javascript/js_testing.js
@@ -0,0 +1,30 @@
+// I do not know how to use javascript.
+// I am just trying to learn it.
+const num1 = 5;
+const num2 = 3;
+const sum = num1 + num2;
+console.log(sum);
+
+// while loop
+whileLoop = () => {
+    let i = 0;
+    while (i <= 5) {
+        console.log(i);
+        i++;
+    }
+}
+
+// for loop
+forLoop = () => {
+    for (let i = 5; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+// if-else statement - not working
+//const age = document.getElementById("age").value;
+//if (age >= 18) {
+//    console.log("You are an adult.");
+//} else {
+//    console.log("You are a child.");
+//}
